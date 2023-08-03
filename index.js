e627ef600239fecdb9f5e065f02a9614c3466c2c
virginiafreitas <virginiacdefreitas@gmail.com>
2023-08-02 23:23:08 -0700

    function view employees

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const db = require('./db/connection');
 
 const inquirer = require('inquirer');
+
+async function viewEmployees() {
+    const employees = await db.promise().query("SELECT * FROM employee")
+    console.table(employees[0])
+}

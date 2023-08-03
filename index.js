8d34b8e690510de1b52e0097c973adf92195f450
virginiafreitas <virginiacdefreitas@gmail.com>
2023-08-02 23:30:07 -0700

    function init choices

--- index.js
+++ index.js
@@ -6,3 +6,31 @@ async function viewEmployees() {
     const employees = await db.promise().query("SELECT * FROM employee")
     console.table(employees[0])
 }
+
+async function init() {
+    const response = await inquirer.prompt([
+        {
+            type: "list",
+            name: "mainChoice",
+            message: "Welcome to Employee Tracker. Please select an option.",
+            choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department"]
+        }
+    ])
+    if (response.mainChoice === "View All Employees") {
+        viewEmployees();
+    } else if (response.mainChoice === "Add Employee") {
+        viewEmployees();
+    } else if (response.mainChoice === "Update Employee Role") {
+        viewEmployees();
+    } else if (response.mainChoice === "View All Roles") {
+        viewEmployees();
+    } else if (response.mainChoice === "Add Role") {
+        viewEmployees();
+    } else if (response.mainChoice === "View All Departments") {
+        viewEmployees();
+    } else (response.mainChoice === "Add Department"); {
+        viewEmployees();
+    }
+}
+
+init();

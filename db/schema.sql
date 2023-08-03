40421b1f368bf92b9bc94a8f30b29676b5470fce
virginiafreitas <virginiacdefreitas@gmail.com>
2023-08-02 23:17:14 -0700

    created db

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+-- drop existing database
+DROP DATABASE IF EXISTS employees_db;
+
+-- create new database 
+CREATE DATABASE employees_db; 
+
+-- use db
+USE employees_db;
+
+-- create 3 tables:
+
+-- create 1st table: department
+
+-- create id and dept name columns
+
+
+-- create 2nd table: role
+
+-- create id**, title, salary and department_id** columns
+
+
+-- create 3rd table: employee
+
+-- create id**, first name, last name, role_id** and manager_id**
+
+
+-- run packages on terminal (already did):
+-- npm init -y
+-- run npm i inquirer@8.2.4 on terminal
+-- run mysql package: npm i mysql2
+
+
+
